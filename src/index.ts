459b430cb8479ab1553dfb5a9f5a00adf1bc9109
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2023-01-24 12:43:49 +0000

    M package.json  M src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,13 @@ import handleError, {
   ErrorMessageOptional as ErrorMessages,
   FormatJSON,
 } from './handleError.js'
+import getErrorInfo from './scripts/getErrorInfo'
 
 export default ReqError
 export {
   catchError,
   handleError,
+  getErrorInfo,
   // Types
   FormatJSON,
   CatchInput,
