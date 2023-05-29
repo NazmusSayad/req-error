12f940eeb5b63111b8dd1c99bb100da310b5e29c
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2023-05-29 21:13:25 +0000

    M package.json  M src/index.ts ?? src/catchError.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import ReqError from './ReqError'
+import catchError from './catchError'
 import checkType from './checkType/checkType'
 import ErrorManager from './ErrorManager'
 
 export * from './types'
 export * from './ExtraError'
-export { checkType, ReqError, ErrorManager }
+export { checkType, catchError, ReqError, ErrorManager }
